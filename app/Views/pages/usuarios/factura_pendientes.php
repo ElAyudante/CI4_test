dd6fe920aafb39c35187048e5fb39edb92d32182
lelexito <lelex-97@hotmail.es>
2022-11-24 15:07:10 +0100

    Update

---
+++ app/Views/pages/usuarios/factura_pendientes.php
@@ -0,0 +1,11 @@
+<?php 
+            if(empty($data)){
+
+            
+        ?>
+            <div class="container p-5 d-flex align-items-center w-50">
+                <h3 class="p-3 text-white text-uppercase fs-1 bg-blue fw-bold mb-0"><img style="width: 60px;" class="img-fluid me-3" src="<?php echo base_url(),'/'; ?>/assets/images/png/logo_white.svg">Actualmente no tiene facturas pendientes a su nombre</h3>
+            </div>
+
+        <?php 
+            } else {}
--- app/Views/pages/usuarios/pagos_pendientes.php
+++ app/Views/pages/usuarios/pagos_pendientes.php
@@ -22,7 +22,7 @@
             
         ?>
             <div class="container p-5 d-flex align-items-center w-50">
-                <h3 class="p-3 text-white text-uppercase fs-1 bg-blue fw-bold mb-0"><img style="width: 60px;" class="img-fluid me-3" src="<?php echo base_url(),'/'; ?>/assets/images/png/logo_white.svg">Actualmente no tiene  pagos pendientes a su nombre</h3>
+                <h3 class="p-3 text-white text-uppercase fs-1 bg-blue fw-bold mb-0"><img style="width: 60px;" class="img-fluid me-3" src="<?php echo base_url(),'/'; ?>/assets/images/png/logo_white.svg">Actualmente no tiene pagos pendientes a su nombre</h3>
             </div>
 
         <?php 
