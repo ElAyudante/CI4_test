c70b49890c5a63eb3dc68d758bf1d325eabc2d98
lelexito <lelex-97@hotmail.es>
2022-11-24 16:37:10 +0100

    Update Form

--- app/Views/pages/usuarios/seguridad.php
+++ app/Views/pages/usuarios/seguridad.php
@@ -20,7 +20,7 @@ if (!$_SESSION['valido'] || $_SESSION['valido']=="false") {
         <div class="container p-5">
           <h3 class="p-3 text-white text-uppercase fs-1 bg-blue fw-bold mb-0">Cambiar Contraseña</h3>
 
-          <?php echo form_open(); ?>
+          <?php echo form_open('users/cambio_contraseña'); ?>
           <div class="form-border p-3 bg-white mb-0">
             
             <div class="row row-cols-lg-4 g-lg-4 cblue text-uppercase">
@@ -28,10 +28,10 @@ if (!$_SESSION['valido'] || $_SESSION['valido']=="false") {
                 <div class="form-group">
                   <strong>Contraseña Actual:</strong>
                   <div class="d-flex">
-                    <input id="password" type="password" class="form-control" name="pass" placeholder="Contraseña Actual">
-                    <span class="input-group-text" onclick="password_show_hide();">
-                      <i class="fas fa-eye" id="show_eye"></i>
-                      <i class="fas fa-eye-slash d-none" id="hide_eye"></i>
+                    <input id="password1" type="password" class="form-control" name="pass" placeholder="Contraseña Actual">
+                    <span class="input-group-text" onclick="password_show_hide1();">
+                      <i class="fas fa-eye" id="show_eye1"></i>
+                      <i class="fas fa-eye-slash d-none" id="hide_eye1"></i>
                     </span>
                   </div>
                 </div>
@@ -41,10 +41,10 @@ if (!$_SESSION['valido'] || $_SESSION['valido']=="false") {
                 <div class="form-group">
                   <strong>Nueva Contraseña:</strong>
                   <div class="d-flex">
-                    <input id="password" type="password" class="form-control" name="pass" placeholder="Nueva Contraseña">
-                    <span class="input-group-text" onclick="password_show_hide();">
-                      <i class="fas fa-eye" id="show_eye"></i>
-                      <i class="fas fa-eye-slash d-none" id="hide_eye"></i>
+                    <input id="password2" type="password" class="form-control" name="pass" placeholder="Nueva Contraseña">
+                    <span class="input-group-text" onclick="password_show_hide2();">
+                      <i class="fas fa-eye" id="show_eye2"></i>
+                      <i class="fas fa-eye-slash d-none" id="hide_eye2"></i>
                     </span>
                   </div>
                 </div>
@@ -54,10 +54,10 @@ if (!$_SESSION['valido'] || $_SESSION['valido']=="false") {
                 <div class="form-group">
                   <strong>Confirmar Contraseña:</strong>
                   <div class="d-flex">
-                    <input id="confirm_password" type="password" class="form-control" name="confirm_pass" placeholder="Confirmar Contraseña">
-                    <span class="input-group-text" onclick="password_show_hide_confirm();">
-                      <i class="fas fa-eye" id="show_eye_confirm"></i>
-                      <i class="fas fa-eye-slash d-none" id="hide_eye_confirm"></i>
+                    <input id="password3" type="password" class="form-control" name="confirm_pass" placeholder="Confirmar Contraseña">
+                    <span class="input-group-text" onclick="password_show_hide3();">
+                      <i class="fas fa-eye" id="show_eye3"></i>
+                      <i class="fas fa-eye-slash d-none" id="hide_eye3"></i>
                     </span>
                   </div>
                 </div>
