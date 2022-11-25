3a510a45c49164044362f27b3e231026e9ad3f86
lelexito <lelex-97@hotmail.es>
2022-11-25 14:20:09 +0100

    Update Email View

--- app/Views/pages/email_plantilla.php
+++ app/Views/pages/email_plantilla.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="X-UA-Compatible" content="IE=edge" /> 
         <title>Logopedas</title>
-        <link rel="stylesheet" href="<?= base_url() ?>/public/assets/grocery_crud/themes/bootstrap-v4/css/bootstrap/bootstrap.min.css">
+        <!--<link rel="stylesheet" href="<?= base_url() ?>/public/assets/grocery_crud/themes/bootstrap-v4/css/bootstrap/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"/>
         <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -20,28 +20,29 @@
         <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js" integrity="sha256-lSjKY0/srUM9BE3dPm+c4fBo1dky2v27Gdjm2uoZaL0=" crossorigin="anonymous"></script>
         <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
         <script src="https://js.stripe.com/v3/"></script>
-        <script src="https://kit.fontawesome.com/fe3a27a4aa.js" crossorigin="anonymous"></script>
+        <script src="https://kit.fontawesome.com/fe3a27a4aa.js" crossorigin="anonymous"></script>-->
 </head>
 <body>
-    <section class="letter container p-5 w-50">
-        <div class="header-letter bg-blue d-flex justify-content-center p-4">
-            <img src="cid:<?= $cid2 ?>" class="img-fluid" style="height:100px">
+    <section class="letter container p-5 w-50" style="width: 50%!important; padding: 3rem!important;">
+
+        <div class="bg-blue d-flex justify-content-center p-4" style="background-color: #156ac0 !important; display: flex!important; justify-content: center!important; padding: 1.5rem!important;">
+            <img src="cid:" class="img-fluid" style="height:100px; max-width: 100%;">
         </div>
-        <div class="body-letter bg-gray text-justify p-5">
-            <h2 class="text-uppercase fw-bold mb-4"><img src="cid:<?= $cid ?>" style="height:40px" alt="">Se añadió un nuevo Pokemon <img src="cid:<?= $cid ?>" style="height:40px" alt=""></h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec augue ex, viverra nec sapien sit amet, ultrices maximus eros. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Praesent felis metus, congue suscipit hendrerit molestie, ultrices a ligula. Fusce scelerisque elit leo. Quisque a fermentum libero. Praesent mattis accumsan eros et bibendum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin viverra accumsan nibh, quis porta dui ultricies finibus. Cras in tincidunt justo. Morbi ac nunc vestibulum, tempor lectus sed, tempor leo. Maecenas sagittis enim id est porta iaculis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-            <div class="d-flex justify-content-center mt-5">
-                <a href="<?php echo base_url(),'/'; ?>users/index_login" class="text-white" target="_blank">
-                    <button class="btn btn-primary text-uppercase">Log in</button>
+        <div class="bg-gray text-justify p-5" style="background-color: #eeeeee !important; text-align: justify; padding: 3rem!important;">
+            <h2 class="text-uppercase fw-bold mb-4" style="font-family: 'Roboto', sans-serif; text-transform: uppercase!important; font-weight: 700!important; margin-bottom: 1.5rem!important;"><img src="cid:" style="height:40px" alt="">Se añadió un nuevo Pokemon <img src="cid:" style="height:40px" alt=""></h2>
+            <p style="font-family: 'Roboto', sans-serif; margin-top: 0; margin-bottom: 1rem;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec augue ex, viverra nec sapien sit amet, ultrices maximus eros. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Praesent felis metus, congue suscipit hendrerit molestie, ultrices a ligula. Fusce scelerisque elit leo. Quisque a fermentum libero. Praesent mattis accumsan eros et bibendum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin viverra accumsan nibh, quis porta dui ultricies finibus. Cras in tincidunt justo. Morbi ac nunc vestibulum, tempor lectus sed, tempor leo. Maecenas sagittis enim id est porta iaculis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+            <div class="d-flex justify-content-center mt-5" style="display: flex!important; justify-content: center!important; margin-top: 3rem!important;">
+                <a href="<?php echo base_url(),'/'; ?>users/index_login" target="_blank">
+                    <button class="btn btn-primary text-uppercase" style="border-radius: 0.375rem; border-width: 1px; padding: 0.375rem 0.75rem; color: white; text-transform: uppercase!important; background-color: #004987; border-color: #004987 !important; font-weight: 900; width: 150px;">Log in</button>
                 </a>                    
             </div>
 
         </div>
-        <div class="footer-letter bg-footer p-4">
-            <div class="d-flex align-items-center justify-content-center my-3">
-                <p class="text-white mb-0 pe-2">Sigue nuestros contenidos de actualidad en:</p>
-                <a href="https://es-es.facebook.com/colegiologopedascantabria/" target="_blank"><img class="me-2" src="cid:<?= $cid3 ?>"></a>
-                <a href="https://es.linkedin.com/in/colegio-logopedas-cantrabria-592675154" target="_blank"><img src="cid:<?= $cid4 ?>"></a>
+        <div class="bg-footer p-4" style="background-image: linear-gradient(#004987, #00a0df ); padding: 1.5rem!important">
+            <div class="d-flex align-items-center justify-content-center my-3" style="display: flex!important; align-items: center!important; justify-content: center!important; margin-top: 1rem!important; margin-bottom: 1rem!important;">
+                <p class="text-white mb-0 pe-2" style="margin-top: 0; font-family: 'Roboto', sans-serif; color: white !important; margin-bottom: 0!important; padding-right: 0.5rem!important;">Sigue nuestros contenidos de actualidad en:</p>
+                <a href="https://es-es.facebook.com/colegiologopedascantabria/" target="_blank"><img class="me-2" style="margin-right: 0.5rem!important;" src="cid:"></a>
+                <a href="https://es.linkedin.com/in/colegio-logopedas-cantrabria-592675154" target="_blank"><img src="cid:"></a>
             </div>
         </div>
     </section>
